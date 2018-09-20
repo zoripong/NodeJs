0a76563a77d0b1a2f4bcddab36a2b5990f668013
Han Yuri <dbfl0406@e-mirim.hs.kr>
2018-09-20 21:51:20 +0900

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+ 
+});
+console.log("실행 포트 번호는 :" + process.env.PORT);
+app.listen(process.env.PORT||3000);
